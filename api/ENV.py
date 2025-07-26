c64ba3990abcadf0e80631f79d77284c13a30a81
Johann Schwabe <johann@schwabe.dev>
2025-07-26 14:22:28 +0200

    no secrets

--- api/ENV.py
+++ api/ENV.py
@@ -21,7 +21,7 @@ DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "iot_db")
 
 # Read password from secret file
-DB_PASSWORD = read_secret_file("/run/secrets/hiot_db_password")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
 
 # Read bot token from secret file
 TELEGRAM_BOT_TOKEN = read_secret_file("/run/secrets/hiot_telegram_bot_token")
